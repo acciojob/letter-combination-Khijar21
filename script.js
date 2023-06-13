d8eb266e55cd7274b0aeaa392613d09b5313779a
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-06-13 21:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+	if(input_digit == ""){
+		return[];
+	}
+	let table = ['0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+
+	let res = [];
+	let que = [''];
+
+	while(que.length > 0){
+		let str = que[0];
+		que.shift();
+
+		if(str.length == input_digit.length){
+			res.push(str)
+		}
+		else {
+			let s = Number(input_digit.charAt(str.length))
+			let val = table[s];
+
+			for(let i = 0; i < val.length; i++){
+				que.push(str+val.charAt(i));
+			}
+		}
+	}
+	return res;
 }
 
 module.exports = letterCombinations;
